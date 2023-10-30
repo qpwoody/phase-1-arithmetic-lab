cad247d845dcc57c36ee7de1681a3cc7774fe6f9
quinn woodhouse <qpwoodhouse@gmail.com>
2023-10-30 16:52:38 -0600

    arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+  
+const random = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER) + 1;
+
+const num3 = 14;
+const num4 = 10;
+const mod = num3 % num4;
+
+const numArray = [3, 4, 8, 20, 11]
+const max = Math.max(...numArray)
